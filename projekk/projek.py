c4c4df464cd5838cf554683a1ef3c297d3e2b6a2
YOUR_NAME <YOUR_EMAIL>
2024-03-12 00:22:16 +0200

    -

--- projekk/projek.py
+++ projekk/projek.py
@@ -15,31 +15,27 @@ yhteys = mysql.connector.connect(
 
 kursori = yhteys.cursor()
 
-npcTypes = ["friendly", "robber", "player"]
 
-tyypit = random.choice(npcTypes)
-määrä = random.randint(1, 3)
+#def lentokenttaSiirto():
+   # valinta = [tulos - 1]
+   # kursori.execute("INSERT INTO game (location) VALUES (%s), (%s);", (valinta))
+   # print(f"U start at the airport {valinta}")
+   # print("Have fun)")
+   # return valinta
 
-kursori.execute(f"UPDATE airport SET npc_type = '{tyypit}' WHERE npc_type IS NULL;")
+#startingPoint()
 
-kursori.fetchall()
+#def arrival():
+   # print("Which one do u want to talk to?")
+   # valinta = input("Type in number (1-3) and press Enter to continue..")
 
-print("Time to play the game")
-print("Press Enter to continue..")
+   # if valinta == "1" or valinta == "2" or valinta == "3":
+  #  return
 
-player_name = input("Enter your name: ")
+#arrival()
 
-user_input = input()
-if user_input == "":
-    print("Choose your starting point")
 
-kursori.execute("SELECT airport.name, iso_country FROM airport ORDER BY RAND() LIMIT 3")
-tulos = (kursori.fetchall())
-
-for x in tulos:
-    index = tulos.index(x)
-    print(f"{index + 1}. {x}")
-
+raha = 1000
 dirk = 0
 life = 0
 bums = ["bum"]
@@ -75,6 +71,8 @@ airport_comment = [
     "During your journey, you encounter a passage where a small group of bums can be observed; "
     "you decide to investigate."
 ]
+
+
 print_airport_comment = random.choice(airport_comment)
 
 def random_airport():
@@ -203,6 +201,175 @@ def bum_encounter(bum_count):
         print("Invalid input, encounter will now end. Better luck next time.")
     bum_count -= 1
     return bum_count
+
+
+def purchase_knife(bum_count):
+    global raha
+    global dirk
+    global bum
+
+    knife_buy = input(f"{bum}: So you wish to purchase a knife huh? \nInput yes / no\n")
+    if knife_buy == 'yes':
+        acquire_a_shank = input(f"{bum}: You can have it for 300. \nInput yes / no\n")
+        if acquire_a_shank == 'yes':
+            print("shank acquired")
+            raha -= 300
+            dirk = 1
+            time.sleep(1)
+            print(f"{bum}: A favorable deal")
+            time.sleep(1)
+            print("Bum takes off and disappears to the crowds. ")
+            time.sleep(2)
+            return bum_count
+        elif acquire_a_shank == 'no':
+            print(f"{bum}: Stop bothering me then??")
+            time.sleep(1)
+            print("Bum takes off and disappears to the crowds. ")
+            time.sleep(2)
+            bum_count -= 1
+            return bum_count
+        else:
+            print("Invalid input, encounter will now end. Better luck next time.")
+            bum_count -= 1
+            return bum_count
+    elif knife_buy == 'no':
+        print(f"{bum}: Stop bothering me then??")
+        bum_count -= 1
+        return bum_count
+    else:
+        print("Invalid input, encounter will now end. Better luck next time.")
+        bum_count -= 1
+        return bum_count
+
+
+def request_information(bum_count):
+    global raha
+    global dirk
+    global life
+
+    inforequest = [1, 2, 3, 4]
+    inforequest_outcome = random.choice(inforequest)
+    if inforequest_outcome == 1:
+        print("The less fortunate individual extends an invitation, "
+              "challenging you to engage in a refined fencing duel. ")
+        time.sleep(3)
+        if dirk == 1:
+            print("You pull your knife out.")
+            time.sleep(1)
+            print("The bum notices the knife and decides to exit the fencing duel by running away.")
+            time.sleep(2)
+        else:
+            print("The call to join was not voluntary and the bum charges towards you")
+            time.sleep(2)
+            time.sleep(1)
+            bumcharge_outcome = random.randint(1, 3)
+            if bumcharge_outcome == 1:
+                print("With agile finesse, you skillfully evaded the oncoming advance of the bum, "
+                      "escaping their charge, and gracefully exited the scene, "
+                      "leaving the duel behind")
+                time.sleep(4)
+            else:
+                dialog_death = [
+                    "After an agile maneuver, the bum successfully struck, leaving me wounded and bleeding.",
+                    "A quick and nimble move from the bum resulted in a successful hit, leaving you wounded.",
+                    "Executing a swift and nimble move, the bum landed a successful strike, leaving you injured.",
+                    "The bum executed a swift maneuver, landing a successful hit that left you wounded.",
+                    "A nimble move from the bum resulted in a successful strike, leaving you injured and bleeding.",
+                    "Executing a swift maneuver, the homeless bum landed a hit, leaving you wounded and bleeding.",
+                    "A speedy move from the bum resulted in a successful strike, leaving you injured and bleeding.",
+                    "A nimble maneuver from the homeless bum resulted in a successful hit, leaving you wounded.",
+                    "With a quick move, the bum executed a hit, causing you to be injured and bleeding.",
+                    "A rapid maneuver from the bum led to a successful strike, leaving you wounded and bleeding.",
+                    "Following a swift move by the bum, a precise strike ensued, resulting in you being injured.",
+                    "After a quick maneuver by the bum, they executed a hit, causing you to be wounded.",
+                    "After a nimble maneuver by the bum, they executed a hit, leaving you wounded and bleeding.",
+                    "Following a rapid maneuver by the bum, a successful hit ensued, resulting in you being wounded.",
+                    "Executing a speedy move, the bum landed a hit, resulting in you being injured and bleeding.",
+                    "In the wake of a swift move by the homeless bum, they struck, leaving you injured and bleeding.",
+                    "In the aftermath of a rapid move by the homeless bum, they struck, causing you to be injured.",
+                    "In the wake of a quick maneuver by the bum, they struck, causing you to be wounded and bleeding.",
+                    "In the aftermath of a nimble maneuver by the homeless bum, they struck, leaving you wounded and.",
+                    "In the wake of a nimble move by the homeless bum, they struck, causing you to be bleeding."
+                ]
+                print_death = random.choice(dialog_death)
+                print(print_death)
+                time.sleep(4)
+                print("You Died. ")
+                life = 1
+
+    elif inforequest_outcome == 2:
+        print(f"{bum}: I am no snitch. ")
+        print(f"{bum} walks away. ")
+        bum_count -= 1
+        return bum_count
+    else:
+        info_buy = input(f"{bum}: I can sell you a clue for 100. \nInput yes / no\n")
+        if info_buy == 'yes':
+            raha -= 100
+            print("clue")
+        elif info_buy == 'no':
+            print(f"{bum}: Stop bothering me then??")
+            time.sleep(1)
+            print(f"{bum} takes off and disappears to the crowds. ")
+            bum_count -= 1
+            return bum_count
+        else:
+            print("Invalid input, encounter will now end. Better luck next time. ")
+            bum_count -= 1
+            return bum_count
+
+
+def beginning():
+    print("Game ✧ Start")
+    print("Your objective: \nFind TheBausses lair and them "
+          "\nvia purchasing information from various bums you encounter on your travels."
+          "\nBeat TheBauss because they are very bad for the climate. ")
+
+
+def airport_visit():
+    print(f"You start from {random_airport()} the airport."
+          "\nThere are a lot of people here. ")
+    time.sleep(1)
+    print(print_airport_comment)
+
+    bum_count = int(random.randint(3, 5))
+    while bum_count > 0:
+        bum_count = bum_encounter(bum_count)
+
+
+def airport_arrive():
+    print(f"You arrive to {random_airport()} the airport."
+          "\nThere are a lot of people here. ")
+    time.sleep(1)
+    print(print_airport_comment)
+
+    bum_count = int(random.randint(3, 5))
+    while bum_count > 0:
+        bum_count = bum_encounter(bum_count)
+
+
+airport_visit()
+
+print_airport_comment = random.choice(airport_comment)
+
+
+print("Time to play the game")
+print("Press Enter to continue..")
+
+player_name = input("Enter your name: ")
+
+user_input = input()
+if user_input == "":
+    print("Choose your starting point")
+
+kursori.execute("SELECT airport.name, iso_country FROM airport ORDER BY RAND() LIMIT 3")
+tulos = (kursori.fetchall())
+
+
+for x in tulos:
+    index = tulos.index(x)
+    print(f"{index + 1}. {x}")
+
 
 def play_game(bum_count):
     print("mustajaakko ei löytyny")
@@ -390,7 +557,6 @@ def purchase_knife(bum_count):
         bum_count -= 1
         return bum_count
 
-
 def request_information(bum_count):
     global raha
     global dirk
@@ -398,6 +564,7 @@ def request_information(bum_count):
 
     inforequest = [1, 2, 3, 4]
     inforequest_outcome = random.choice(inforequest)
+
     if inforequest_outcome == 1:
         print("The less fortunate individual extends an invitation, "
               "challenging you to engage in a refined fencing duel. ")
@@ -445,11 +612,13 @@ def request_information(bum_count):
                 time.sleep(4)
                 print("You Died. ")
                 life = 1
+
     elif inforequest_outcome == 2:
         print(f"{bum}: I am no snitch. ")
         print(f"{bum} walks away. ")
         bum_count -= 1
         return bum_count
+
     else:
         info_buy = input(f"{bum}: I can sell you a clue for 100. \nInput yes / no\n")
         if info_buy == 'yes':
@@ -468,108 +637,13 @@ def request_information(bum_count):
 
 def beginning():
     print("Game ✧ Start")
-    print("Your objective: \nFind TheBausses lair and them "
+    print("Your objective: \nFind Taylor Swifts lair and them "
           "\nvia purchasing information from various bums you encounter on your travels."
-          "\nBeat TheBauss because they are very bad for the climate. ")
-
-def airport_visit():
-    print(f"You start from {random_airport()} the airport."
-          "\nThere are a lot of people here. ")
-    time.sleep(1)
-    print(print_airport_comment)
-
-    bum_count = int(random.randint(3, 5))
-    while bum_count > 0:
-        bum_count = bum_encounter(bum_count)
-
-
-def airport_arrive():
-    print(f"You arrive to {random_airport()} the airport."
-          "\nThere are a lot of people here. ")
-    time.sleep(1)
-    print(print_airport_comment)
-
-    bum_count = int(random.randint(3, 5))
-    while bum_count > 0:
-        bum_count = bum_encounter(bum_count)
-
-start = input("Type in number (1-3) and press Enter to continue..")
-
-def startingPoint(start):
-    valinta = tulos[int(start) - 1]
-    kursori.execute("INSERT INTO game (location) VALUES (%s), (%s);", (valinta))
-    print(f"U start at the airport {valinta}")
-    print("Have fun)")
-    return valinta
-
-startingPoint(start)
-def robbery():
-    chance = 0.75
-    arpa = float(random.randint(0, 1))
-
-    if arpa > chance:
-        print("Local is willing to rob u, can u protect urself?")
-        syöte = input("Press 'K' to use a knife ")
-
-        if syöte == "K":
-            kursori.execute("SELECT Knife FROM inventory")
-            puukko = kursori.fetchone()
-
-            if puukko[0] > 0:
-                print("U pull out ur knife and threaten robber with it, he walks away.. Press Enter to continue..")
-                kursori.execute("UPDATE inventory SET Knife = Knife - 1 WHERE Knife > 0")
-        else:
-            kursori.execute("UPDATE inventory SET money = money * 0.5")
-
-            kursori.execute("SELECT money FROM inventory")
-            rahaTilanne = kursori.fetchone()[0]
-
-            print("Damn it, I didn't have my knife with me..")
-            print("Luckily I always hide half of my money in my socks")
-            print(f"Well.. now I have {rahaTilanne} ")
-    else:
-        chooseNpc()
-
-    return
-class Player:
-    pass
-
-class Friendly:
-    def tryToBuyCode(self):
-        print("U know anything about THE CODE? ")
-        # ei midii hajuu viel
-
-listOfNpc = [Friendly,Player]
-
-def chooseNpc():
-
-    npc = random.choice(listOfNpc)
-
-    if npc == Friendly:
-        olio = Friendly
-        print("U meet friendly npc and have dialogue")
-        #dialogue metodi
-
-    elif npc == Player:
-        Olio = Player
-        print("U've met local card player, down to play?")
-        #metodi card game
-    return npc
-
-def arrival():
-    print(f"U see {random.randint(1,3)} people")
-    print("Which one do u want to talk to?")
-    valinta = input("Type in number (1-3) and press Enter to continue..")
-
-    if valinta == "1" or valinta == "2" or valinta == "3":
-        chooseNpc()
-    return
-
-arrival()
+          "\nBeat Taylor Swift because they are very bad for the climate. ")
 
 #boss fight
 def play_russian_roulette():
-    print('"Welcome to Bausses lair!"')
+    print('"Welcome to Swifts lair!"')
     print("\"I like to play with high stakes..so let's play some Russian Roulette HAHA!\"")
     print("You start by pulling the trigger.")
 
@@ -589,11 +663,11 @@ def play_russian_roulette():
             chambers -= 1
 
         if chambers == 1:
-            print("Click! You survived! You know the boss will have to admit defeat now.")
+            print("Click! You survived! You know Taylor will have to admit defeat now.")
             time.sleep(1)
-            print("Well after all this, I am an honorable man.")
+            print("Well after all this, I am an honorable woman.")
             time.sleep(1)
-            print("BANG! TheBaus pulls the trigger on himself")
+            print("BANG! Taylor pulls the trigger on herself")
             time.sleep(1)
             print("You win! Congratulations!")
             time.sleep(1)
@@ -601,18 +675,21 @@ def play_russian_roulette():
             time.sleep(1)
             break
 
-        print("Bausses turn")
+        print("Taylor's turn")
         time.sleep(1)
-        print('"You ready for your demice?"')
+        print("You ready for your demice?")
         time.sleep(1)
 
 
         if chambers == bullet_chamber:
-            print("BANG! TheBaus loses!")
+            print("BANG! Swift sucks!")
             time.sleep(1)
             break
+
         else:
-            print("*The boss pulls the trigger")
+            print("*Taylor pulls the trigger")
             time.sleep(1)
-            print("Click! The boss lives that one.")
+            print("Click! Taylor lives that one. :( ")
             chambers -= 1
+
+beginning()
