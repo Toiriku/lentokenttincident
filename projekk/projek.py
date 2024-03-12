bec335dad77dd971726b5997d7404119cb4953ba
rikut <riku.toivanen123@gmail.com>
2024-03-12 15:42:50 +0200

    -ä

--- projekk/projek.py
+++ projekk/projek.py
@@ -380,7 +380,7 @@ def play_game(bum_count):
     bet = 1
 
     def jako(deck):
-        käsi = []
+        kasi = []
         for i in range(2):
             random.shuffle(deck)
             kortti = random.choice(deck)
@@ -388,12 +388,12 @@ def play_game(bum_count):
             if kortti == 12: kortti = "Q"
             if kortti == 13: kortti = "K"
             if kortti == 14: kortti = "A"
-            käsi.append(kortti)
-        return käsi
+            kasi.append(kortti)
+        return kasi
 
-    def total(käsi):
+    def total(kasi):
         total = 0
-        for kortti in käsi:
+        for kortti in kasi:
             if kortti == "J" or kortti == "Q" or kortti == "K":
                 total += 10
             elif kortti == "A":
@@ -408,8 +408,8 @@ def play_game(bum_count):
     def play_again():
         again = input("Do you want to play again Y/N?").lower()
         if again == "y":
-            dealer_käsi = []
-            player_käsi = []
+            dealer_kasi = []
+            player_kasi = []
             deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14] * 4
             game()
         else:
@@ -426,49 +426,49 @@ def play_game(bum_count):
             kortti = "K"
         if kortti == 14:
             kortti = "A"
-        käsi.append(kortti)
-        return käsi
+        kasi.append(kortti)
+        return kasi
 
-    def print_result(dealer_käsi, player_käsi):
+    def print_result(dealer_kasi, player_kasi):
         clear()
 
         print("Time to play blackjack")
-        print("Dealer has a " + str(dealer_käsi) + "for a total of " + str(total(dealer_käsi)))
-        print("Player has a" + str(player_käsi) + "for a total of " + str(total(player_käsi)))
+        print("Dealer has a " + str(dealer_kasi) + "for a total of " + str(total(dealer_kasi)))
+        print("Player has a" + str(player_kasi) + "for a total of " + str(total(player_kasi)))
 
-    def blackjack(dealer_käsi, player_käsi):
-        if total(player_käsi) == 21:
-            print_result(player_käsi, dealer_käsi)
+    def blackjack(dealer_kasi, player_kasi):
+        if total(player_kasi) == 21:
+            print_result(player_kasi, dealer_kasi)
             print("You got a blackjack!")
-        elif total(dealer_käsi) == 21:
-            print_result(dealer_käsi, player_käsi)
+        elif total(dealer_kasi) == 21:
+            print_result(dealer_kasi, player_kasi)
             print("The dealer has a blackjack")
             raha -= bet
 
-    def score(dealer_käsi, player_käsi):
-        if total(player_käsi) == 21:
-            print_result(dealer_käsi, player_käsi)
+    def score(dealer_kasi, player_kasi):
+        if total(player_kasi) == 21:
+            print_result(dealer_kasi, player_kasi)
             print("You got a blackjack!")
-        elif total(dealer_käsi) == 21:
-            print_result(dealer_käsi, player_käsi)
+        elif total(dealer_kasi) == 21:
+            print_result(dealer_kasi, player_kasi)
             print("The dealer has a blackjack")
-        elif total(player_käsi) > 21:
-            print_result(dealer_käsi, player_käsi)
+        elif total(player_kasi) > 21:
+            print_result(dealer_kasi, player_kasi)
             print("You bust")
-        elif total(dealer_käsi) > 21:
-            print_result(dealer_käsi, player_käsi)
+        elif total(dealer_kasi) > 21:
+            print_result(dealer_kasi, player_kasi)
             print("The dealer busts")
             raha += bet * 1.5
-        elif total(player_käsi) < total(dealer_käsi):
-            print_result(dealer_käsi, player_käsi)
+        elif total(player_kasi) < total(dealer_kasi):
+            print_result(dealer_kasi, player_kasi)
             print("The dealer has a higher score")
             raha -= bet
-        elif total(player_käsi) > total(dealer_käsi):
-            print_result(dealer_käsi, player_käsi)
+        elif total(player_kasi) > total(dealer_kasi):
+            print_result(dealer_kasi, player_kasi)
             print("You have a higher score")
             raha += bet * 1.5
-        elif total(player_käsi) == total(dealer_käsi):
-            print_result(dealer_käsi, player_käsi)
+        elif total(player_kasi) == total(dealer_kasi):
+            print_result(dealer_kasi, player_kasi)
             print("It's a tie")
             raha -= bet
 
@@ -488,30 +488,30 @@ def play_game(bum_count):
     def game():
         choice = 0
         print("Time to game")
-        dealer_käsi = jako(deck)
-        player_käsi = jako(deck)
-        print("dealer has " + str(dealer_käsi[0]))
+        dealer_kasi = jako(deck)
+        player_kasi = jako(deck)
+        print("dealer has " + str(dealer_kasi[0]))
         make_bet()
-        print("You have a " + str(player_käsi[0]))
-        blackjack(dealer_käsi, player_käsi)
+        print("You have a " + str(player_kasi[0]))
+        blackjack(dealer_kasi, player_kasi)
         quit = False
         while not quit:
             choice = input("Do you want to [H]it, [S]tand or [Q]uit").lower()
             if choice == 'h':
-                hit(player_käsi)
-                if total(player_käsi) > 21:
+                hit(player_kasi)
+                if total(player_kasi) > 21:
                     print("You bust")
                     raha -= bet
                     play_again()
                 elif choice == 's':
-                    while total(dealer_käsi) < 17:
-                        hit(dealer_käsi)
-                        print(dealer_käsi)
-                        if total(dealer_käsi) > 21:
+                    while total(dealer_kasi) < 17:
+                        hit(dealer_kasi)
+                        print(dealer_kasi)
+                        if total(dealer_kasi) > 21:
                             print("Dealer busts")
                             raha += bet * 1.5
                             play_again()
-                        score(dealer_käsi, player_käsi)
+                        score(dealer_kasi, player_kasi)
                         play_again()
                 elif choice == 'q':
                     print("Thanks for the game")
